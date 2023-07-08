6421781e22ab340591f87b16b66221727beb2760
Niwaiwa <po2846@gmail.com>
2023-07-09 02:06:04 +0800

    feat: add request id and logger format
    add logger format
    add request id and middleware
    add asgi-correlation-id package

--- app/config.py
+++ app/config.py
@@ -1,4 +1,10 @@
+import logging
 from pydantic import BaseSettings
+from functools import lru_cache
+from logging.config import dictConfig
+
+
+logger = logging.getLogger('app')
 
 class Settings(BaseSettings):
     secret_key: str = "secret"
@@ -7,4 +13,49 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
-        
+
+@lru_cache()
+def get_settings():
+    return Settings()
+
+LOGGIN_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {  # correlation ID filter must be added here to make the %(correlation_id)s formatter work
+        'correlation_id': {
+            '()': 'asgi_correlation_id.CorrelationIdFilter',
+            'uuid_length': 32,
+            'default_value': '-',
+        },
+    },
+    'formatters': {
+        'console': {
+            'class': 'logging.Formatter',
+            # 'datefmt': '%H:%M:%S',
+            # formatter decides how our console logs look, and what info is included.
+            # adding %(correlation_id)s to this format is what make correlation IDs appear in our logs
+            'format': '%(levelname)s: [%(asctime)s] %(name)s:%(module)s:%(lineno)d [%(correlation_id)s] %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            # Filter must be declared in the handler, otherwise it won't be included
+            'filters': ['correlation_id'],
+            'formatter': 'console',
+        },
+    },
+    # Loggers can be specified to set the log-level to log, and which handlers to use
+    'loggers': {
+        # project logger
+        'app': {'handlers': ['console'], 'level': 'DEBUG', 'propagate': True},
+        'uvicorn': {'handlers': ['console'], 'level': 'DEBUG', 'propagate': False},
+        # third-party package loggers
+        # 'databases': {'handlers': ['console'], 'level': 'WARNING'},
+        # 'httpx': {'handlers': ['console'], 'level': 'INFO'},
+        'asgi_correlation_id': {'handlers': ['console'], 'level': 'WARNING'},
+    },
+}
+
+def configure_logging() -> None:
+    dictConfig(LOGGIN_CONFIG)
--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
-from functools import lru_cache
+import logging
+
+from app import config
+from asgi_correlation_id import CorrelationIdMiddleware
 from fastapi import FastAPI, Depends
 from typing_extensions import Annotated
-from app import config
+from app.config import get_settings
 
-app = FastAPI()
+logger = logging.getLogger('app')
 
-@lru_cache()
-def get_settings():
-    return config.Settings()
+app = FastAPI(on_startup=[config.configure_logging])
+app.add_middleware(CorrelationIdMiddleware)
 
 @app.get("/")
 def read_root():
@@ -15,6 +17,7 @@ def read_root():
 
 @app.get("/info")
 async def info(settings: Annotated[config.Settings, Depends(get_settings)]):
+    logger.info("info")
     return {
         "debug": settings.debug,
     }
