703708f57cef8154843e870d8d035986ccd7c0d2
Niwaiwa <po2846@gmail.com>
2023-07-08 23:24:00 +0800

    feat: downgrade to fastapi 0.99
    downgrande fastapi version to 0.99

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
-from pydantic_settings import BaseSettings
+from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     secret_key: str = "secret"
     debug: bool = False
     db_url: str = "postgresql://postgres:password@localhost:5432/vara"
 
-settings = Settings()
+    class Config:
+        env_file = ".env"
+        
--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
-from fastapi import FastAPI
-from configs.config import settings
+from functools import lru_cache
+from fastapi import FastAPI, Depends
+from typing_extensions import Annotated
+from app import config
 
 app = FastAPI()
+
+@lru_cache()
+def get_settings():
+    return config.Settings()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.get("/info")
-async def info():
-    return "test"
+async def info(settings: Annotated[config.Settings, Depends(get_settings)]):
+    return {
+        "debug": settings.debug,
+    }
