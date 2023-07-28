d99133e176974e488466d6d06952fe991e09a028
Niwaiwa <po2846@gmail.com>
2023-07-29 03:23:31 +0800

    feat: add friend status to profile api

--- app/api/v1/endpoints/profile.py
+++ app/api/v1/endpoints/profile.py
@@ -10,8 +10,12 @@ from app.core.security import create_access_token
 from app.core.deps import get_current_user, get_current_user_optional
 from app.config import get_settings
 from app.usecase.following_usecase import FollowingUseCase
+from app.usecase.friend_usecase import FriendUseCase
+from app.usecase.friend_request_usecase import FriendRequestUseCase
 from app.usecase.user_usecase import UserUseCase
 from app.repositories.following_repository import FollowingRepository
+from app.repositories.friend_repository import FriendRepository
+from app.repositories.friend_request_repository import FriendRequestRepository
 from app.repositories.user_repository import UserRepository
 from app.db.database import get_db_connection
 
@@ -36,10 +40,17 @@ def read_user_by_username(
     
     is_following = False
     is_follower = False
+    is_friend = False
+    is_friend_request = False
     if current_user and current_user.username != username:
         following_usercase = FollowingUseCase(FollowingRepository(db))
         is_following = following_usercase.is_following(current_user.id, user.id)
         is_follower = following_usercase.is_follower(current_user.id, user.id)
+
+        friend_usercase = FriendUseCase(FriendRepository(db))
+        is_friend = friend_usercase.is_friend(current_user.id, user.id)
+        friend_request_usercase = FriendRequestUseCase(FriendRequestRepository(db))
+        is_friend_request = friend_request_usercase.is_friend_request(current_user.id, user.id)
 
     profile = schemas.ProfileResponse(
         id=user.id,
@@ -50,6 +61,8 @@ def read_user_by_username(
         description=user.description,
         is_following=is_following,
         is_follower=is_follower,
+        is_friend=is_friend,
+        is_friend_request=is_friend_request,
     )
     logger.info(f"profile: {profile}")
     return profile
